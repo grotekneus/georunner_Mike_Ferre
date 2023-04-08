f8d1ce295c213f0cbda121ad4acb4e547aedf961
grotekneus <ferre.theys@student.uhasselt.be>
2023-04-08 13:26:48 +0200

    added navdrawer template

--- app/src/main/java/com/example/georunner/MainActivity.kt
+++ app/src/main/java/com/example/georunner/MainActivity.kt
@@ -1,12 +1,30 @@
 package com.example.georunner
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.georunner.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var menuBarToggle: ActionBarDrawerToggle
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
-    var bad = 3
+    private fun setupMenuDrawer() {
+        menuBarToggle = ActionBarDrawerToggle(this,binding.drawerLayout, R.string.menu_open, R.string.menu_close)
+        binding.drawerLayout.addDrawerListener(menuBarToggle)
+        menuBarToggle.syncState()
+
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+
+
+        }
+    }
 }
