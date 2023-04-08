a9b4e5305053a9a72dc0b5f53bd6bc22a9fcd416
grotekneus - Ferre Theys <125285581+grotekneus@users.noreply.github.com>
2023-04-08 13:48:35 +0200

    updated homeactivity to resolve conflict

--- app/src/main/java/com/example/georunner/HomeActivity.kt
+++ app/src/main/java/com/example/georunner/HomeActivity.kt
@@ -6,11 +6,24 @@ import com.example.georunner.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHomeBinding
+    private lateinit var menuBarToggle: ActionBarDrawerToggle
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityHomeBinding.inflate((layoutInflater))
         setContentView(binding.root)
+        
+        
+    private fun setupMenuDrawer() {
+        menuBarToggle = ActionBarDrawerToggle(this,binding.drawerLayout, R.string.menu_open, R.string.menu_close)
+        binding.drawerLayout.addDrawerListener(menuBarToggle)
+        menuBarToggle.syncState()
 
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+
+
+        }
     }
 }
