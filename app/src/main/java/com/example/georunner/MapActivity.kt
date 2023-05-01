70abc5aca9c1a4631a054a67c63725eb9074fcfc
grotekneus <ferre.theys@student.uhasselt.be>
2023-05-01 16:29:22 +0200

    refixed location tracking

--- app/src/main/java/com/example/georunner/MapActivity.kt
+++ app/src/main/java/com/example/georunner/MapActivity.kt
@@ -1,28 +1,86 @@
 package com.example.georunner
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.os.Bundle
-import androidx.annotation.NonNull
 import androidx.appcompat.app.AppCompatActivity
-import com.example.georunner.databinding.ActivityMainBinding
-import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.OnMapReadyCallback
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.georunner.databinding.ActivityMapBinding
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
 
-class MapActivity : AppCompatActivity(),OnMapReadyCallback{
+
+class MapActivity : AppCompatActivity(),OnMapReadyCallback {
     private lateinit var binding: ActivityMapBinding
+    private lateinit var map: GoogleMap
+    private val REQUEST_LOCATION_PERMISSION = 1
+    lateinit var locationManager : LocationManager
 
     @Override
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMapBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        locationManager= getSystemService(Context.LOCATION_SERVICE) as LocationManager
 
         var mapFragment = supportFragmentManager.findFragmentById(R.id.maps) as SupportMapFragment
         mapFragment.getMapAsync(this)
 
     }
 
-    override fun onMapReady(@NonNull googleMap:GoogleMap){}
 
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
+        checkPermissions(locationManager)
+
+    }
+    fun checkPermissions(locationManager: LocationManager){
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED ||
+            ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0f, locationListener)
+            map.isMyLocationEnabled = true
+        } else {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf<String>(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ),
+                REQUEST_LOCATION_PERMISSION
+            )
+
+        }
+    }
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        if (requestCode == REQUEST_LOCATION_PERMISSION) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission granted
+            } else {
+                // Permission denied
+            }
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        }
+    }
+    val locationListener = object : LocationListener {
+        override fun onLocationChanged(location: Location) {
+            val cameraUpdate = CameraUpdateFactory.newLatLngZoom(LatLng(location.latitude, location.longitude), 15f)
+            map.animateCamera(cameraUpdate)
+        }
+
+        override fun onProviderDisabled(provider: String) {}
+
+        override fun onProviderEnabled(provider: String) {}
+
+        override fun onStatusChanged(provider: String, status: Int, extras: Bundle) {}
+    }
 }
